617773aa34542571b89b472ac0214ce2166c6606
PhamPhong <phamphong9981@gmail.com>
2025-07-05 17:36:25 +0000

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: process.env.BASEPATH
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  }
 }
 
 export default nextConfig
