1399d9ea02e16c681c6617c180f7858a76930141
PhamPhong <phamphong9981@gmail.com>
2025-07-05 16:52:49 +0000

    fix lint

--- src/app/(blank-layout-pages)/layout.tsx
+++ src/app/(blank-layout-pages)/layout.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 // Type Imports
 import type { ChildrenType } from '@core/types'
 
@@ -7,12 +5,18 @@ import type { ChildrenType } from '@core/types'
 import Providers from '@components/Providers'
 import BlankLayout from '@layouts/BlankLayout'
 
+// Util Imports
+import { getMode, getSettingsFromCookie } from '@core/utils/serverHelpers'
+
+
 const Layout = ({ children }: ChildrenType) => {
   // Vars
   const direction = 'ltr'
+  const mode = getMode()
+  const settingsCookie = getSettingsFromCookie()
 
   return (
-    <Providers direction={direction}>
+    <Providers direction={direction} mode={mode} settingsCookie={settingsCookie}>
       <BlankLayout>{children}</BlankLayout>
     </Providers>
   )
