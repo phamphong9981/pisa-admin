cec4b8d3fa52520452935329c02327c74036663e
PhamPhong <phamphong9981@gmail.com>
2026-01-22 04:11:50 +0000

    fix

--- src/@layouts/components/vertical/LayoutContent.tsx
+++ src/@layouts/components/vertical/LayoutContent.tsx
@@ -9,13 +9,20 @@ import type { ChildrenType } from '@core/types'
 // Util Imports
 import { verticalLayoutClasses } from '@layouts/utils/layoutClasses'
 
+// Hook Imports
+import useVerticalNav from '@menu/hooks/useVerticalNav'
+
 // Styled Component Imports
 import StyledMain from '@layouts/styles/shared/StyledMain'
 
 const LayoutContent = ({ children }: ChildrenType) => {
+  // Hooks
+  const { isToggled } = useVerticalNav()
+
   return (
     <StyledMain
       isContentCompact={true}
+      isNavToggled={isToggled}
       className={classnames(verticalLayoutClasses.content, verticalLayoutClasses.contentCompact, 'flex-auto is-full')}
     >
       {children}
--- src/@layouts/styles/shared/StyledMain.tsx
+++ src/@layouts/styles/shared/StyledMain.tsx
@@ -6,15 +6,23 @@ import themeConfig from '@configs/themeConfig'
 
 type StyledMainProps = {
   isContentCompact: boolean
+  isNavToggled?: boolean
 }
 
 const StyledMain = styled.main<StyledMainProps>`
-  padding: ${themeConfig.layoutPadding}px;
-  ${({ isContentCompact }) =>
+  padding: ${({ isNavToggled }) => (isNavToggled ? 0 : themeConfig.layoutPadding)}px;
+  ${({ isContentCompact, isNavToggled }) =>
     isContentCompact &&
+    !isNavToggled &&
     `
     margin-inline: auto;
     max-inline-size: ${themeConfig.compactContentWidth}px;
+  `}
+  ${({ isNavToggled }) =>
+    isNavToggled &&
+    `
+    width: 100%;
+    max-width: 100%;
   `}
 `
 
